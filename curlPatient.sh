af8a4ba52e754902f7ca3fab18cd4e52992dacea
elsalant <salant@il.ibm.com>
2023-03-02 17:16:26 +0200

    fixed URL

--- curlPatient.sh
+++ curlPatient.sh
@@ -1,1 +1,1 @@
-curl http://127.0.01/myrecords -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiUGF0aWVudCIsImlkIjoiMDg3MjNkOTctOGRkMy00NDgxLWE1ZjEtYTk0Mjc0ODhkNzI5In0.HquBhDRV78iz59DtmLzkytciCvy3No-SNODSiVemxwU"
+curl http://127.0.0.1:5559/myrecords -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiUGF0aWVudCIsImlkIjoiMDg3MjNkOTctOGRkMy00NDgxLWE1ZjEtYTk0Mjc0ODhkNzI5In0.HquBhDRV78iz59DtmLzkytciCvy3No-SNODSiVemxwU"
